94f5e02c5a9f6db7f76f0b17d49d881cc5d93b4f
aryan <roaringmoon07@gmail.com>
2026-06-03 09:25:53 +0000

    Add example of hybrid inheritance with classes A, B, and C

---
+++ hybrid.py
@@ -0,0 +1,14 @@
+#write an eg code for hybrid inheritance in python class A: and use pass as placeholder for the body of the class. class B: and use pass as placeholder for the body of the class. class C(A, B): and use pass as placeholder for the body of the class. create an object of class C and print a message "hello world from hybrid inheritance" --- IGNORE ---
+
+
+class A:
+    pass
+
+class B:
+    pass
+
+class C(A, B):
+    pass
+
+obj = C()
+print("hello world from hybrid inheritance")

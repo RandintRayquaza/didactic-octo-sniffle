3e09784a52f11c45eee2745972b0873794b5e3ca
aryan <roaringmoon07@gmail.com>
2026-06-03 09:43:25 +0000

    Refactor demo function to demonstrate polymorphism with multiple definitions

--- poloymorephoism.py
+++ poloymorephoism.py
@@ -1,1 +1,14 @@
 #it is a process of making an oprator or method to work on one or more oprations 
+def demo():
+    print("Polymorphism")
+
+def demo(a,b):
+    return a+b
+mid = demo
+def demo(a,b,c):
+    return a*b*c
+
+
+print(demo(2,3,4))
+
+print(mid(2,3))

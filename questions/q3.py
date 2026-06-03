25d68f8bd62ee63c5f23913e536cdf433ccc7986
aryan <roaringmoon07@gmail.com>
2026-06-03 09:22:07 +0000

    Implement Calculator class with __init__ method for addition, subtraction, and multiplication

--- questions/q3.py
+++ questions/q3.py
@@ -20,3 +20,44 @@ calc.add(10, 5)
 calc.sub(10, 5)
 calc.multi(10, 5)
 
+
+
+
+#uisng init method
+
+class Addition: 
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        print(self.a + self.b)
+
+class Subtraction:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def sub(self):
+        print(self.a - self.b)
+
+class Multiplication:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def multi(self):
+        print(self.a * self.b)
+
+class Calculator(Addition, Subtraction, Multiplication):
+    def __init__(self, a, b):
+        Addition.__init__(self, a, b)
+        Subtraction.__init__(self, a, b)
+        Multiplication.__init__(self, a, b)
+
+
+calc = Calculator(10, 5)
+calc.add()
+calc.sub()
+calc.multi()
+

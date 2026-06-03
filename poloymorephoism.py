997472d4ac00dfbf6645b2be837f24ac747fe424
aryan <roaringmoon07@gmail.com>
2026-06-03 10:13:44 +0000

    Add class-based salary calculation example demonstrating polymorphism

--- poloymorephoism.py
+++ poloymorephoism.py
@@ -16,3 +16,27 @@ print(demo(2,3,4))
 print(mid(2,3))
 
 var()
+
+
+#code eg using class
+
+class Employee:
+    def calculate_salary(self):
+        pass
+
+class Manager(Employee):
+    def calculate_salary(self):
+        return 5000
+
+class intern(Employee):
+    def calculate_salary(self):
+        return 2000
+
+
+
+sal = [Manager(), intern(),]
+
+for i in sal:
+    print(i.calculate_salary())
+
+

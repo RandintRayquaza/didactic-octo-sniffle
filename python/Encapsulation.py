118f681671c25a0090074bddad7ccc01da2360d6
Aryan <roaringmoon07@gmail.com>
2026-06-04 06:09:03 +0000

    Refactor Bank class to improve encapsulation and add balance validation methods

--- python/Encapsulation.py
+++ python/Encapsulation.py
@@ -3,8 +3,23 @@ class Bank:
     def __init__(self, bal, accno):
         self.__bal = bal
         self.__accno = accno
-
-
-c1 = Bank(34000, 37848)
-print(c1.__bal)
+    def get_bal(self):
+        return self.__bal
     
+    def __validate_ammt(self,ammt):
+        return ammt>0
+        
+
+
+    def set_bal(self,ammt):
+        if self.__validate_ammt(ammt):
+            self.__bal = ammt
+            print(f'New amount{self.__bal}')
+        
+
+c1 = Bank(23347,3473)
+c1.__bal = 90000
+c1.set_bal(40000)
+print(c1.get_bal())
+
+
